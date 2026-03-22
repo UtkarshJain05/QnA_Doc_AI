bf9d7f3120154867508626a3e3e3dd7e706d19ab
Utkarsh Jain <jain.utkarsh.5605@gmail.com>
2026-03-22 16:36:26 +0530

    Changes for Mobile Screen fix.

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -88,7 +88,7 @@ const ChatPage = () => {
 
   return (
     // FIX 1: Changed h-screen to h-[100dvh] and added overflow-hidden to lock the background
-    <div className="fixed inset-0 flex w-full flex-col overflow-hidden bg-background">
+    <div className="fixed inset-0 flex flex-col w-full overflow-hidden bg-background">
       <WaveBackground />
       <HeaderBar onNewPdf={handleNewPdf} onEndSession={handleEndSession} />
 
